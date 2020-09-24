091a8261da74b0f81da6942138cf2bd8418b10eb
Norman Sánchez Nolasco <normansanchezn@gmail.com>
2020-09-23 23:55:48 -0500

    [ADD] Configuración del proyecto

--- tlist/app/src/main/java/com/examen/tlist/MainActivity.java
+++ tlist/app/src/main/java/com/examen/tlist/MainActivity.java
@@ -1,7 +1,11 @@
 package com.examen.tlist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.examen.tlist.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
---
+++ tlist/app/src/main/java/com/examen/tlist/ui/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.examen.tlist.ui.login;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.examen.tlist.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        }
+}
